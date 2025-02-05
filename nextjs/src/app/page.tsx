5e46ddfc01025dd84539dca06ada0ab55e14591d
Hunter Pruett <hunterpruett2003@gmail.com>
2025-02-04 18:37:02 -0800

    factored link for NextJS

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <Link
+        href="/dashboard"
+        className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
+      >
+        Go to Dashboard
+      </Link>
+    </main>
+  );
 }
